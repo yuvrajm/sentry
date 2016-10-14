f006b6a8f7ffc9793aa5a29da81b0533af320d70
Matt Robenolt <matt@ydekproductions.com>
2016-10-14 11:58:09 -0700

    sourcemaps: conditionally import libsourcemap if found

--- src/sentry/lang/javascript/sourcemaps/__init__.py
+++ src/sentry/lang/javascript/sourcemaps/__init__.py
@@ -1,3 +1,17 @@
 from __future__ import absolute_import
 
-from .native import View  # NOQA
+from .native import View as NativeSourceMapView
+
+try:
+    from libsourcemap import View as LibSourceMapView, IndexedSourceMap
+
+    class View(object):
+        @staticmethod
+        def from_json(buffer):
+            try:
+                return LibSourceMapView.from_json(buffer)
+            except IndexedSourceMap:
+                return NativeSourceMapView.from_json(buffer)
+
+except ImportError:
+    View = NativeSourceMapView
